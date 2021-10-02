f0cd9e6d8d67c8b7032239cf18c036b5653f73f5
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-10-02 17:30:46 +0900

    ➕[ADD] : 프로젝트 파일 추가 (#1)

---
+++ Youtube-Clone/Youtube-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube-Clone
+//
+//  Created by 김윤서 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
