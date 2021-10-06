87ddad3d5066160801c07ca670cde73a82afddde
yoonseo <yo26on@soongsil.ac.kr>
2021-10-06 15:40:21 +0900

    [#1] ✨[FEAT] : 회원가입 VC , Welcome VC 구현
    - 회원가입 VC , Welcome VC 구현
    - Consts (idenetifier) 분리
    - setBackgroundColor Extension 추가
    - SignIn -> Login 으로 네이밍 변경
    - 도전과제 구현 완료

---
+++ Youtube-Clone/Youtube-Clone/Sources/Consts/Const.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Youtube-Clone
+//
+//  Created by 김윤서 on 2021/10/06.
+//
+
+enum Const {}
---
+++ Youtube-Clone/Youtube-Clone/Sources/Consts/Storyboard.swift
@@ -0,0 +1,12 @@
+//
+//  Storyboard.swift
+//  Youtube-Clone
+//
+//  Created by 김윤서 on 2021/10/06.
+//
+
+extension Const {
+    enum Storyboard {
+        static let auth = "Auth"
+    }
+}
