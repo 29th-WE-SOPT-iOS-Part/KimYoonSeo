36aab28085c3061b9571b2c8f7cd0e6fb6203d98
yoonseo <yo26on@soongsil.ac.kr>
2021-10-28 00:39:56 +0900

    ✨[FEAT] : HomeViewController TableView 구현 (#5)

---
+++ Youtube-Clone/Youtube-Clone/Global/Model/Video.swift
@@ -0,0 +1,13 @@
+//
+//  Video.swift
+//  Youtube-Clone
+//
+//  Created by 김윤서 on 2021/10/27.
+//
+
+struct Video {
+    let preview: String
+    let profile: String
+    let title: String
+    let subtitle: String
+}
