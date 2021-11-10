2f33c0fa56519c1daf74cc97badec474248b0c6c
yoonseo <yo26on@soongsil.ac.kr>
2021-11-11 03:27:03 +0900

    ✨[FEAT] :  로그인 flow present 방식 수정  (#9)

--- Youtube-Clone/Youtube-Clone/Global/Coordinator/LoginCoordinator.swift
+++ Youtube-Clone/Youtube-Clone/Global/Coordinator/LoginCoordinator.swift
@@ -24,22 +24,61 @@ class LoginCoordinator: Coordinator {
         self.presenter = presenter
         self.childCoordinators = []
     }
+
+    let navigationController = UINavigationController()
     
     func start() {
         let loginVC = LoginViewController()
         loginVC.coordinator = self
-        self.presenter.pushViewController(loginVC, animated: true)
+        navigationController.setViewControllers([loginVC], animated: true)
+        navigationController.modalPresentationStyle = .fullScreen
+        self.presenter.present(navigationController, animated: true, completion: nil)
     }
 }
 
 extension LoginCoordinator: LoginViewControllerDelegate {
     func loginButtonDidTapped(_ loginViewController: LoginViewController, name: String?) {
-        self.delegate?.goToSignInViewController(self, name: name)
+        let signInCoordinator = SignInCoordinator(presenter: self.navigationController)
+        signInCoordinator.delegate = self
+        signInCoordinator.name = name
+        signInCoordinator.start()
+        self.addChildCoordinator(signInCoordinator)
     }
     
     func signUpButtonDidTapped(_ loginViewController: LoginViewController) {
-        self.delegate?.goToSignUpViewController(self)
+        let signUpCoordinator = SignUpCoordinator(presenter: self.navigationController)
+        signUpCoordinator.delegate = self
+        signUpCoordinator.start()
+        self.addChildCoordinator(signUpCoordinator)
     }
-    
-    
+}
+
+extension LoginCoordinator: SignUpCoordinatorDelegate {
+    func goToTabbarController(_ signUpViewController: SignUpCoordinator, name: String?) {
+        let signInCoordinator = SignInCoordinator(presenter: self.navigationController)
+        signInCoordinator.delegate = self
+        signInCoordinator.name = name
+        signInCoordinator.start()
+        self.addChildCoordinator(signInCoordinator)
+    }
+}
+
+extension LoginCoordinator: SignInCoordinatorDelegate {
+    func goToSignUpViewController(_ signInCoordinator: SignInCoordinator) {
+        signInCoordinator.rootViewController.dismiss(animated: true) {
+            let loginCoordinator = LoginCoordinator(presenter: self.presenter)
+            loginCoordinator.start()
+            self.addChildCoordinator(loginCoordinator)
+            self.removeChildCoordinator(signInCoordinator)
+        }
+    }
+
+    func goToTabbarController(_ signInCoordinator: SignInCoordinator) {
+        self.navigationController.dismiss(animated: true) {
+            self.removeChildCoordinator(signInCoordinator)
+        }
+        self.presenter.dismiss(animated: true) {
+            self.removeChildCoordinator(self)
+        }
+    }
 }
