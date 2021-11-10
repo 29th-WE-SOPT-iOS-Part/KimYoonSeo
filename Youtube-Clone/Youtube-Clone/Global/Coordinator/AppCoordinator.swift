58f40876154bf85aed317879564f7dafc9770bba
yoonseo <yo26on@soongsil.ac.kr>
2021-11-11 03:27:03 +0900

    ✨[FEAT] :  로그인 flow present 방식 수정  (#9)

--- Youtube-Clone/Youtube-Clone/Global/Coordinator/AppCoordinator.swift
+++ Youtube-Clone/Youtube-Clone/Global/Coordinator/AppCoordinator.swift
@@ -31,56 +31,10 @@ final class AppCoordinator: Coordinator{
     }
 }
 
-extension AppCoordinator: LoginCoordinatorDelegate {
-    func goToSignInViewController(_ loginCoorninator: LoginCoordinator, name: String?) {
-        let signInCoordinator = SignInCoordinator(presenter: self.presenter)
-        signInCoordinator.delegate = self
-        signInCoordinator.name = name
-        signInCoordinator.start()
-        self.addChildCoordinator(signInCoordinator)
-    }
-    
-    func goToSignUpViewController(_ loginCoorninator: LoginCoordinator) {
-        let signUpCoordinator = SignUpCoordinator(presenter: self.presenter)
-        signUpCoordinator.delegate = self
-        signUpCoordinator.start()
-        self.addChildCoordinator(signUpCoordinator)
-    }
-    
-}
-
-extension AppCoordinator: SignUpCoordinatorDelegate {
-    func goToTabbarController(_ signUpViewController: SignUpCoordinator, name: String?) {
-        let signInCoordinator = SignInCoordinator(presenter: self.presenter)
-        signInCoordinator.delegate = self
-        signInCoordinator.name = name
-        signInCoordinator.start()
-        self.addChildCoordinator(signInCoordinator)
-    }
-}
-
-extension AppCoordinator: SignInCoordinatorDelegate {
-    func goToSignUpViewController(_ signInCoordinator: SignInCoordinator) {
-        signInCoordinator.rootViewController.dismiss(animated: true) {
-            let loginCoordinator = LoginCoordinator(presenter: self.presenter)
-            loginCoordinator.delegate = self
-            loginCoordinator.start()
-            self.addChildCoordinator(loginCoordinator)
-            self.removeChildCoordinator(signInCoordinator)
-        }
-    }
-    
-    func goToTabbarController(_ signInCoordinator: SignInCoordinator) {
-        self.start()
-    }
-}
-
 extension AppCoordinator: TabBarCoodinatorDelegate {
     func goToLoginViewController(_ coordinator: TabBarCoodinator) {
-        self.removeChildCoordinator(coordinator)
         let loginCoordinator = LoginCoordinator(presenter: self.presenter)
         loginCoordinator.start()
-        loginCoordinator.delegate = self
         self.addChildCoordinator(loginCoordinator)
     }
 }
